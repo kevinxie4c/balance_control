6c39ace5b3b8e8136e514851d26b839f3e8e9a9e
Kaixiang Xie <kaixiang.xie@mail.mcgill.ca>
2024-03-29 16:25:37 -0400

    fix an error in copy.pl

--- copy.pl
+++ copy.pl
@@ -4,4 +4,4 @@ use File::Copy;
 my $suffix = shift @ARGV;
 copy "model/actor-$suffix.par", "model/actor.par";
 copy "model/critic-$suffix.par", "model/critic.par";
-copy "model/state_normalizer-$suffix.nd", "state_normalizer.nd";
+copy "model/state_normalizer-$suffix.nd", "model/state_normalizer.nd";
