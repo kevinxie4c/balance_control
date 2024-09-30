51bbdaf367da679d88982ba7d4d5e0886dfb2829
Kaixiang Xie <kaixiang.xie@mail.mcgill.ca>
2024-09-30 17:32:31 -0400

    SIM_MODE for AntEnv

--- CharacterEnv/cpp/src/AntEnv.cpp
+++ CharacterEnv/cpp/src/AntEnv.cpp
@@ -4,6 +4,8 @@
 #include "AntEnv.h"
 #include "IOUtil.h"
 
+#define SIM_MODE 0
+
 using namespace std;
 using namespace Eigen;
 using namespace dart::dynamics;
@@ -59,7 +61,7 @@ AntEnv::AntEnv(const char *cfgFilename)
     double cfm = dart::constraint::JointLimitConstraint::getConstraintForceMixing();
     double erp = dart::constraint::JointLimitConstraint::getErrorReductionParameter();
     cfm = 0.001;
-    erp = 0.1;
+    erp = 0.2;
     dart::constraint::JointLimitConstraint::setConstraintForceMixing(cfm);
     dart::constraint::JointLimitConstraint::setErrorReductionParameter(erp);
     dart::constraint::ContactConstraint::setConstraintForceMixing(cfm);
@@ -88,7 +90,7 @@ void AntEnv::reset()
     world->reset();
     VectorXd zeros = VectorXd::Zero(skeleton->getNumDofs());
     VectorXd initPos(skeleton->getNumDofs());
-    initPos << 0, 0, 0, 0, -0.04, 0,
+    initPos << 0, 0, 0, 0, -0.25, 0,
             0, -0.0, 0.0,
             0, 0.0, -0.0,
             0, -0.0, 0.0,
@@ -108,10 +110,116 @@ void AntEnv::step()
     force.tail(action.size()) = action.array() * scales.array();
     //force.setZero();
     //cout << force.transpose() << endl;
+    double h = 1.0 / forceRate;
+
+#if SIM_MODE == 1
+    VectorXd q = skeleton->getPositions();
+    VectorXd dq = skeleton->getVelocities();
+    MatrixXd aS = MatrixXd::Zero(action.size(), action.size());
+    aS.diagonal() = scales;
+    MatrixXd sS = MatrixXd::Zero(state.size(), state.size());
+    sS.diagonal() = (normalizerStd.array() + 1e-8).matrix().cwiseInverse();
+    MatrixXd dsdq = MatrixXd::Zero(36, 18);
+    for (size_t i = 0; i < 18; ++i)
+        dsdq(i, i) = 1;
+    MatrixXd J = MatrixXd::Zero(18, 18);
+    J.bottomRows(12) = aS * policyJacobian * sS * dsdq;
+#endif
+
     for (size_t i = 0; i < forceRate / actionRate; ++i)
     {
+#if SIM_MODE == 0
+        VectorXd q = skeleton->getPositions();
+        VectorXd dq = skeleton->getVelocities();
+        VectorXd ddq = skeleton->getAccelerations();
+        MatrixXd M = skeleton->getMassMatrix();
+        VectorXd C = skeleton->getCoriolisAndGravityForces();
+        //cout << "M\n" << M << endl;
+        //cout << "C\n" << C << endl;
+        MatrixXd aS = MatrixXd::Zero(action.size(), action.size());
+        aS.diagonal() = scales;
+        MatrixXd sS = MatrixXd::Zero(state.size(), state.size());
+        sS.diagonal() = (normalizerStd.array() + 1e-8).matrix().cwiseInverse();
+        //cout << "sS\n" << sS << endl;
+        MatrixXd dsdq = MatrixXd::Zero(36, 18);
+        for (size_t i = 0; i < 18; ++i)
+            dsdq(i, i) = 1;
+        MatrixXd J = MatrixXd::Zero(18, 18);
+        J.bottomRows(12) = aS * policyJacobian * sS * dsdq;
+        //cout << "pJ\n" << policyJacobian << endl;
+        //cout << "J\n" << J << endl;
+        //MatrixXd K(2, 2);
+        //MatrixXd D(2, 2);
+        //K << 500, 0,
+        //     0, 500;
+        //D << 0.0, 0.0,
+        //     0.0, 0.0;
+        //force = -K * q - D * dq;
+        //MatrixXd A = (M + h * h * K);
+        MatrixXd A = (M - h * h * J);
+        VectorXd b = M * dq + h * (force - C);
+        //MatrixXd A = M;
+        //VectorXd b = (M - h * h * J) * dq + h * (force - C);
+        //cout << "A\n" << A << endl;
+        //cout << "b\n" << b << endl;
+        //VectorXd dq_n = A.inverse() * b;
+        VectorXd dq_n = A.fullPivLu().solve(b);
+        //VectorXd q_n = q + h * dq_n;
+        //q_n = q_n.cwiseMax(lowerLimits);
+        //q_n = q_n.cwiseMin(upperLimits);
+        //dq_n = (q_n - q) / h;
+        //skeleton->setPositions(q_n);
+        //if (dq_n.norm() == 0)
+        //{
+        //    cout << "policyJacobian:\n" << policyJacobian << endl;
+        //    cout << "aS:\n" << aS << endl;
+        //    cout << "sS:\n" << sS << endl;
+        //    cout << "A:\n" << A << endl;
+        //    cout << "M:\n" << M << endl;
+        //    cout << "J:\n" << J << endl;
+        //    cout << "b:\n" << b << endl;
+        //    cout << "dq:\n" << endl;
+        //    cout << "force:\n" << force << endl;
+        //    cout << "C:\n" << C << endl;
+        //    exit(0);
+        //}
+        skeleton->setVelocities(dq_n);
+        //cout << "dq_n: " << dq_n.transpose() << endl;
+        world->getConstraintSolver()->solve();
+        if (skeleton->isImpulseApplied())
+        {
+          skeleton->computeImpulseForwardDynamics();
+          skeleton->setImpulseApplied(false);
+        }
+        skeleton->integratePositions(h);
+        //cout << "v: " << skeleton->getVelocities().transpose() << endl;
+        //cout << "p: " << skeleton->getPositions().transpose() << endl;
+        if (skeleton->getPositions().array().isNaN().any() || skeleton->getVelocities().array().isNaN().any())
+        {
+            done = true;
+            cout << "done" << endl;
+            skeleton->setPositions(q);
+            skeleton->setVelocities(dq);
+            break;
+        }
+        world->setTime(world->getTime() + h);
+#elif SIM_MODE == 1
+        VectorXd f = force + i * h * J * dq;
+        skeleton->setForces(f);
+        //cout << "f: " << f.transpose() << endl;
+        world->step();
+        if (skeleton->getPositions().array().isNaN().any() || skeleton->getVelocities().array().isNaN().any())
+        {
+            done = true;
+            cout << "done" << endl;
+            skeleton->setPositions(q);
+            skeleton->setVelocities(dq);
+            break;
+        }
+#else
         skeleton->setForces(force);
         world->step();
+#endif
         dart::collision::CollisionResult result = world->getLastCollisionResult();
         if (result.inCollision(skeleton->getBodyNodes()[0]))
         {
