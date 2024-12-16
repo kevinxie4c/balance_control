ea23890d2744b63250dd3cd7baefa21be56c98d5
Kaixiang Xie <kaixiang.xie@mail.mcgill.ca>
2024-12-16 14:34:37 -0500

    SimpleEnv: add refMotion, modify reward

--- CharacterEnv/cpp/src/SimpleEnv.h
+++ CharacterEnv/cpp/src/SimpleEnv.h
@@ -9,6 +9,7 @@ class SimpleEnv: public CharacterEnv
 {
     public:
         SimpleEnv(const char *cfgFilename);
+        ~SimpleEnv();
         void reset() override;
         void step() override;
         void updateState();
@@ -19,6 +20,9 @@ class SimpleEnv: public CharacterEnv
         dart::dynamics::SkeletonPtr floor;
         Eigen::VectorXd scales;
         Eigen::Vector3d prev_com;
+
+        std::vector<Eigen::VectorXd> refMotion;
+        int frameRate;
 };
 
 #endif
